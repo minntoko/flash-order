e4936955babb81719ad070805765d727edda2488
minntoko <minntoko0822@gmail.com>
2024-03-08 20:30:49 +0900

    fix: 使用していないimportを削除

--- src/utils/OpacityTransition.tsx
+++ src/utils/OpacityTransition.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import { easeIn, motion } from "framer-motion";
 
 const OpacityTransition = ({ children }: { children: ReactNode }) => {
